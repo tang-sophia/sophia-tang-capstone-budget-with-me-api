2a3a54e4755540fe7d005c78ff848289049ef4fe
Sophia Tang <tang.sophia.st@gmail.com>
2025-02-19 21:22:30 -0600

    set up get, find, add, delete Budget entries, add migration file to include expense as a column

---
+++ migrations/20250220024711_add_expense_to_budget.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+  await knex.schema.alterTable("budget", (table) => {
+    table.string("expense");
+  });
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function down(knex) {
+  await knex.schema.alterTable("budget", (table) => {
+    table.dropColumn("expense");
+  });
+}
