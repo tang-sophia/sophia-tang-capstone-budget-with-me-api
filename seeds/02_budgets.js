f0a2a8fae69eaebd1820a47b4297cc74e21d6206
Sophia Tang <tang.sophia.st@gmail.com>
2025-02-20 23:43:50 -0600

    update seed file to include more items

--- seeds/02_budgets.js
+++ seeds/02_budgets.js
@@ -6,10 +6,53 @@ export async function seed(knex) {
   // Deletes ALL existing entries
   await knex("budget").del();
   await knex("budget").insert([
-    { expense: "Fruits and vegetables", category: "Groceries", amount: 80.0 },
-    { expense: "Movies", category: "Entertainment", amount: 70.0 },
-    { expense: "Netflix", category: "Subscriptions", amount: 30.0 },
-    { expense: "Gym Membership", category: "Subscriptions", amount: 75.0 },
-    { expense: "Rent", category: "Housing", amount: 300.0 },
+    { expense: "Electricity Bill", category: "Utilities", amount: 120.0 },
+    {
+      expense: "Groceries - Vegetables",
+      category: "Groceries",
+      amount: 25.0,
+    },
+    {
+      expense: "Netflix Subscription",
+      category: "Subscriptions",
+      amount: 15.0,
+    },
+    {
+      expense: "Gas",
+      category: "Transportation",
+      amount: 40.0,
+    },
+    { expense: "Loan Repayment", category: "Loans", amount: 200.0 },
+    {
+      expense: "Credit Card Payment",
+      category: "Credit Card",
+      amount: 150.0,
+    },
+    {
+      expense: "House Maintenance",
+      category: "Maintenance & Repair",
+      amount: 100.0,
+    },
+    { expense: "Health Insurance", category: "Health", amount: 180.0 },
+    {
+      expense: "Children's School Supplies",
+      category: "Children",
+      amount: 60.0,
+    },
+    {
+      expense: "Gifts",
+      category: "Miscellaneous",
+      amount: 30.0,
+    },
+    { expense: "Water Bill", category: "Utilities", amount: 45.0 },
+    { expense: "Meat", category: "Groceries", amount: 35.0 },
+    { expense: "Public Transport", category: "Transportation", amount: 50.0 },
+    { expense: "Loan Interest", category: "Loans", amount: 25.0 },
+    {
+      expense: "Gym Membership",
+      category: "Subscriptions",
+      amount: 50.0,
+    },
+    ,
   ]);
 }
